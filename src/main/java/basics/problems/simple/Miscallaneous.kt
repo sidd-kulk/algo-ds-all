c0ac7e2d60478b60d18fb1e7db07947d7a909ba6
Siddharth Kulkarni <ext_skulkarni@falabella.cl>
2021-09-20 00:17:56 +0530

    Brute force solution for NPair sum in array

--- src/main/java/basics/problems/simple/Miscallaneous.kt
+++ src/main/java/basics/problems/simple/Miscallaneous.kt
@@ -2,6 +2,7 @@ package basics.problems.simple
 
 import org.junit.jupiter.api.Assertions.*
 import java.util.*
+import org.paukov.combinatorics3.Generator;
 
 /**
  * Given an array of positive integers, find the index of a pair of numbers that add up
@@ -24,13 +25,13 @@ import java.util.*
 
 fun pairAddingUpToBruteForce(array: Array<Int>, sum: Int): Optional<Array<Int>> {
     val empty = Optional.empty<Array<Int>>()
-    if(array.isNullOrEmpty() || array.size == 1) {
+    if (array.isNullOrEmpty() || array.size == 1) {
         return empty;
     }
 
-    for(i in 1 until array.size){
+    for (i in 1 until array.size) {
         for (j in i until array.size) {
-            if(array[i] + array[j] == sum) {
+            if (array[i] + array[j] == sum) {
                 return Optional.of(arrayOf(i, j))
             }
         }
@@ -39,11 +40,35 @@ fun pairAddingUpToBruteForce(array: Array<Int>, sum: Int): Optional<Array<Int>> 
 }
 
 
+fun nPairAddingUpTo(array: Array<Int>, nPair: Int, sum: Int): Optional<List<Int>> {
+    /**
+     * [5, 6, 1, 2, 8, 3], 4, 14 ==> 1,2,8,3
+     */
+
+    if (array.isEmpty() || nPair == 0 || nPair > array.size) {
+        return Optional.empty()
+    }
+    val allCombinations: List<MutableList<Int>> = combinations(array, nPair)
+    val result = allCombinations.filter { it.sum() == sum }
+    println(result)
+    return if(result.isNotEmpty()) Optional.of(result.first()) else Optional.empty()
+}
+
+fun factorial(n: Int): Int = if (n < 1) 1 else (1..n).reduce { acc, num -> acc * num }
+fun numberOfCombinations(n: Int, r: Int): Int = factorial(n) / (factorial(r) * factorial(n - r))
+fun combinations(numbers: Array<Int>, k: Int): List<MutableList<Int>> {
+    return Generator.combination(*numbers).simple(k).toList()
+}
+
+
 /********************  TESTS ******************************/
 fun main() {
 //    pairAddingUpTo(arrayOf(5, 2, 6, 3, 1), 9)
-    Tests.runAll()
-    println("Tests Passed!")
+//    Tests.runAll()
+//    println(numberOfCombinations(10, 7))
+//    println("Tests Passed!")
+//    println(combinations(arrayOf(5, 2, 6, 3, 1), 2))
+    nPairAddingUpTo(arrayOf(5, 6, 1, 2, 8, 3), 4, 14).ifPresent(System.out::println)
 }
 
 class Tests {
