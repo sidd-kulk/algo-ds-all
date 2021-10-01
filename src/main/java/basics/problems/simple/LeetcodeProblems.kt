f84feaabd41417305d7bad0f4132135d163204bb
Siddharth Kulkarni <ext_skulkarni@falabella.cl>
2021-10-01 19:34:45 +0530

    Water problem

--- src/main/java/basics/problems/simple/LeetcodeProblems.kt
+++ src/main/java/basics/problems/simple/LeetcodeProblems.kt
@@ -1,7 +1,21 @@
 package basics.problems.simple
 
-// Remove Duplicates in a Sorted Array
+import kotlin.math.abs
+import kotlin.math.min
 
-fun removeDuplicates(inputArray: Array<Int>){
+fun waterFillinBarChart(inputArray: Array<Int>){
+    var waters = 0
+    for(i in inputArray.indices) {
+        for(j in i+1 until inputArray.size){
+            val w = min(inputArray[i], inputArray[j]) * abs(j - i)
+            if(w > waters) {
+                waters = w
+            }
+        }
+    }
+    println(waters)
+}
 
+fun main() {
+    waterFillinBarChart(arrayOf(1,3,7,9,2))
 }
