dccb673c3a83add5fce7f42cd0937bb3222ec560
Siddharth Kulkarni <ext_skulkarni@falabella.cl>
2021-10-04 22:45:04 +0530

    Water problem - Optimized solution with O(n)

--- src/main/java/basics/problems/simple/LeetcodeProblems.kt
+++ src/main/java/basics/problems/simple/LeetcodeProblems.kt
@@ -1,9 +1,12 @@
 package basics.problems.simple
 
 import kotlin.math.abs
+import kotlin.math.max
 import kotlin.math.min
 
-fun waterFillinBarChart(inputArray: Array<Int>){
+//https://leetcode.com/problems/container-with-most-water/
+
+fun waterFillingBarChart(inputArray: Array<Int>){
     var waters = 0
     for(i in inputArray.indices) {
         for(j in i+1 until inputArray.size){
@@ -16,6 +19,25 @@ fun waterFillinBarChart(inputArray: Array<Int>){
     println(waters)
 }
 
+fun waterFillingBarChartOptimized(heights: IntArray): Int {
+    var maxArea = 0
+    var leftPointer = 0
+    var rightPointer = heights.size-1
+    while(leftPointer < rightPointer) {
+        val area = min(heights[leftPointer], heights[rightPointer]) * (rightPointer - leftPointer)
+        maxArea = max(area, maxArea)
+        if(heights[leftPointer] <= heights[rightPointer]){
+            leftPointer++
+        }else{
+            rightPointer--
+        }
+    }
+    return maxArea
+}
+
+
 fun main() {
-    waterFillinBarChart(arrayOf(1,3,7,9,2))
+//    waterFillingBarChart(arrayOf(7,1,2,3,9))
+    println(waterFillingBarChartOptimized(intArrayOf(7,1,2,3,9)))
+    println(waterFillingBarChartOptimized(intArrayOf(1,4,6,5,3,7,6)))
 }
