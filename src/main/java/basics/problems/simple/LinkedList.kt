568f6fc689424108a2a4f5c0ece3c4a58fac4ef5
Siddharth Kulkarni <sidd.kulk@Siddharths-MacBook-Pro.local>
2021-10-24 16:42:37 +0530

    Reversing linked list

--- src/main/java/basics/problems/simple/LinkedList.kt
+++ src/main/java/basics/problems/simple/LinkedList.kt
@@ -1,26 +1,47 @@
 package basics.problems.simple
 
+import java.util.*
 
-class Node<T>(var data: T?, var next: Node<T>?) {
-    fun hasNext(): Boolean {
-        return next != null
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+object Solution {
+    fun reverseList(head: ListNode?): ListNode? {
+        if(head?.next == null) return head
+
+        var cn = head
+        var pn: ListNode? = null
+        while(cn != null) {
+            val nn = cn.next
+            cn.next = pn
+            pn = cn
+            cn = nn
+        }
+        return pn
     }
 }
 
-class LinkedList<T> {
-    private var rootNode: Node<T>? = null
+fun main() {
+}
 
-    fun add(t: T) {
-        if(rootNode == null){
-            rootNode = Node(t, null)
-        } else {
-
-            val newNode = Node(t, null)
-            var tempNode = rootNode
-            while(tempNode?.hasNext() == true){
-                tempNode = tempNode.next!!
-            }
-            tempNode?.next = newNode
+object ReverseLinkedList {
+    fun reverseSinglyLinkedList(linkedList: LinkedList<Int>): LinkedList<Int> {
+        val size = linkedList.size
+        val array = IntArray(size)
+        val iter = linkedList.listIterator()
+        while (iter.hasNext()) {
+            val element = iter.next()
+            val index = iter.nextIndex()
+            array[size - index - 1] = element
         }
+
+        val reversedLinkedList = LinkedList<Int>()
+        for (i in array) {
+            reversedLinkedList.add(i)
+        }
+
+        return reversedLinkedList
     }
 }
