c944506da5afdbedc0891901199324fbb7530e3a
Siddharth Kulkarni <sidd.kulk@Siddharths-MacBook-Pro.local>
2021-10-31 20:44:21 +0530

    Reversing linked list

--- src/main/java/basics/problems/simple/LinkedList.kt
+++ src/main/java/basics/problems/simple/LinkedList.kt
@@ -3,6 +3,45 @@ package basics.problems.simple
 import java.util.*
 
 
+class LLinkedList<T>() {
+    private var rootNode = NNode<T>()
+    fun add(t: T) {
+        val nNode = NNode(t)
+        if(rootNode.next == null) {
+            rootNode.next = nNode
+        } else {
+            var currNode: NNode<T>? = rootNode
+            while(currNode?.next != null) {
+                currNode = currNode.next
+            }
+            currNode?.next = nNode
+        }
+    }
+
+    override fun toString(): String {
+        var currNode: NNode<T>? = rootNode
+        val sb = StringBuilder()
+        while(currNode != null) {
+            sb.append("($currNode)-->")
+            currNode = currNode.next
+        }
+        return sb.toString()
+    }
+}
+
+class NNode<T>(var data: T? = null, var next: NNode<T>? = null) {
+    override fun toString(): String {
+        return if(this.data != null) this.data.toString() else "null"
+    }
+}
+
+fun main() {
+    val l = LLinkedList<Int>()
+    l.add(2)
+    l.add(3)
+    println(l)
+}
+
 class ListNode(var `val`: Int) {
     var next: ListNode? = null
 }
@@ -21,9 +60,6 @@ object Solution {
         }
         return pn
     }
-}
-
-fun main() {
 }
 
 object ReverseLinkedList {
