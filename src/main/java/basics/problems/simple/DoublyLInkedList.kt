44d1e54d96af1989e094c8a014ebde23f1b9e55a
Siddharth Kulkarni <sidcool1234@gmail.com>
2021-11-05 20:17:08 +0530

    Doubly Linked List, flatten hierarchy with recursive solution

--- src/main/java/basics/problems/simple/DoublyLInkedList.kt
+++ src/main/java/basics/problems/simple/DoublyLInkedList.kt
@@ -13,7 +13,7 @@ typealias Node = DoublyLInkedListNode
 
 // https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
 object FlattenHierarchy {
-    fun flatten(root: Node?): Node? {
+    fun flattenIterative(root: Node?): Node? {
         var currNode: Node? = root
         while (currNode != null) {
             if (currNode.child != null) {
@@ -31,6 +31,33 @@ object FlattenHierarchy {
             }
             currNode = currNode.next
         }
+        return root
+    }
+    fun flatten(root: Node?): Node? {
+        fun flattenRec(node: Node?): Node? {
+            var curr: Node? = node
+            var tail: Node? = node
+            while(curr != null) {
+                val child = curr.child
+                val next: Node? = curr.next
+                if(child != null) {
+                    val _tail = flattenRec(child)
+                    _tail?.next = next
+                    next?.prev = _tail
+
+                    curr.next = child
+                    child.prev = curr
+
+                    curr.child = null
+                    curr = tail
+                } else {
+                    curr = next
+                }
+                if(curr != null) tail = curr
+            }
+            return tail
+        }
+        if(root != null) flattenRec(root)
         return root
     }
 }
