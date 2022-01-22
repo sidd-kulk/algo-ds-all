9a639fdb9aa445e3949ed7c65573ffd71362bf54
Siddharth Kulkarni <sidcool1234@gmail.com>
2022-01-22 18:22:46 +0530

    Count of elements more than n/k occurrences - Optimal solution using Moore's voting Algorithm

--- src/main/java/basics/geekforgeeks/CountMoreThanNByKOccurrences.kt
+++ src/main/java/basics/geekforgeeks/CountMoreThanNByKOccurrences.kt
@@ -33,4 +33,41 @@ object CountMoreThanNByKOccurrences {
         return output.toIntArray()
     }
 
+    /**
+     *  In case the the nums array is very huge and k is very small,
+     *  then the hashmap will be very huge.  Solution to use space optimally
+     */
+    // Moore's voting algorithm
+    // COPY PASTING JAVA SOLUTION AS IT COULD NOT BE SOLVED BY ME.  MAY BE LATER.  SIGH!!!
+    fun countOptimalWithBetterMemory(nums: IntArray, k: Int): IntArray {
+        // [30,10,20,20,10,20,30,30], k=4
+        /*HashMap<Integer,Integer> m=new HashMap<Integer,Integer>();
+
+        for(int i=0;i<n;i++){
+            if(m.containsKey(arr[i])){
+                int count=m.get(arr[i]);
+                m.put(arr[i],count+1);
+            }
+            else if(m.size()<k-1)
+                m.put(arr[i],1);
+            else
+                for(Map.Entry x:m.entrySet()){
+            Integer c=(Integer)x.getValue();
+            m.put((Integer)x.getKey(),c-1);
+            if((Integer)x.getKey()==0)
+                m.remove(x.getKey());}
+        }
+        for(Map.Entry x:m.entrySet()){
+            int count=0;
+            for(int i=0;i<n;i++){
+            if((Integer)x.getKey()==arr[i])
+                count++;
+
+        }
+            if(count>n/k)
+                System.out.print(x.getKey()+" ");
+        }*/
+        TODO()
+    }
+
 }
