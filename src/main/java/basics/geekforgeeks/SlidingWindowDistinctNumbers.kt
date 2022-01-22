af6fb4b40363c8d4d34c1a2e864ec2870c58b430
Siddharth Kulkarni <sidcool1234@gmail.com>
2022-01-22 11:53:41 +0530

    Distict numbers in a sliding window - Optimal Solution

--- src/main/java/basics/geekforgeeks/SlidingWindowDistinctNumbers.kt
+++ src/main/java/basics/geekforgeeks/SlidingWindowDistinctNumbers.kt
@@ -18,4 +18,40 @@ object SlidingWindowDistinctNumbers {
 
         return output.toIntArray()
     }
+
+    fun countOptimal(nums: IntArray, k: Int): IntArray {
+        /**
+         *  Trick: First only iterate over the first window and add its size to the output array
+         *  Then iterate over the rest of items starting k
+         *  For each iteration, update the frequency map.  If any frequency becomes 0, remove the item.
+         */
+        if (k > nums.size) return intArrayOf()
+        if (nums.size <= 1) return intArrayOf(nums.size)
+
+        val output = mutableListOf<Int>()
+
+        val map = mutableMapOf<Int, Int>()
+
+        for(i in 0 until k){
+            if (map.containsKey(nums[i])) {
+                map[nums[i]] = map[nums[i]]!! + 1
+            } else {
+                map[nums[i]] = 1
+            }
+        }
+        output.add(map.size)
+        for(i in k until nums.size) {
+            map[nums[i-k]] = map[nums[i-k]]!! - 1
+            if(map[nums[i-k]] == 0) map.remove(nums[i-k])
+
+            if (map.containsKey(nums[i])) {
+                map[nums[i]] = map[nums[i]]!! + 1
+            } else {
+                map[nums[i]] = 1
+            }
+
+            output.add(map.size)
+        }
+        return output.toIntArray()
+    }
 }
--- src/test/java/basics/geekforgeeks/SlidingWindowDistinctNumbersTest.kt
+++ src/test/java/basics/geekforgeeks/SlidingWindowDistinctNumbersTest.kt
@@ -18,4 +18,17 @@ internal class SlidingWindowDistinctNumbersTest {
             assertArrayEquals(it.first, SlidingWindowDistinctNumbers.countBruteForce(it.second, it.third))
         }
     }
+
+    @Test
+    fun countOptimal() {
+        arrayOf(
+            Triple(intArrayOf(2,3,4,3), intArrayOf(10,20,20,10,30,40,10), 4),
+            Triple(intArrayOf(1,1), intArrayOf(10,10,10,10), 3),
+            Triple(intArrayOf(3,4,3), intArrayOf(10,10,5,3,20,5), 4),
+            Triple(intArrayOf(), intArrayOf(), 4),
+            Triple(intArrayOf(1), intArrayOf(1), 1)
+        ).forEach {
+            assertArrayEquals(it.first, SlidingWindowDistinctNumbers.countOptimal(it.second, it.third))
+        }
+    }
 }
