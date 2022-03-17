fd47a3aa101859a94dc46c5eb2eed61aa0683df6
Siddharth Kulkarni <sidcool1234@gmail.com>
2022-03-17 22:19:20 +0530

    Basic Recursion techniques

--- src/main/java/basics/geekforgeeks/Recursion.kt
+++ src/main/java/basics/geekforgeeks/Recursion.kt
@@ -1,13 +1,23 @@
 package basics.geekforgeeks
 
 fun binary(n: Int) {
-    if(n == 0) {
+    if (n == 0) {
         return
     }
-    binary(n/2)
-    print(n%2)
+    binary(n / 2)
+    print(n % 2)
+}
+
+fun printNums(n: Int, order: String = "regular") {
+    if(n == 0){
+        return
+    }
+    if(order == "reversed") print("$n,")
+    printNums(n - 1, order)
+    if(order == "regular") print("$n,")
 }
 
 fun main() {
-    binary(13)
+//    printNums(13)
+    printNums(13, "reversed")
 }
