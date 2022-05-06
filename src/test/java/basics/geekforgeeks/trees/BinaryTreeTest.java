9f06857e13caba86fdd524431bf353107538c914
Siddharth Kulkarni <sidcool1234@gmail.com>
2022-05-06 14:17:29 +0530

    Java Reboot

---
+++ src/test/java/basics/geekforgeeks/trees/BinaryTreeTest.java
@@ -0,0 +1,12 @@
+package basics.geekforgeeks.trees;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinaryTreeTest {
+
+    @Test
+    void maxDepth() {
+    }
+}
