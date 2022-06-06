49117332ebf16754d7f178876dcd8a898248d48f
Siddharth Kulkarni <sidcool1234@gmail.com>
2022-06-06 22:01:38 +0530

    Binary Search - Index of first occurrence in a sorted array

--- src/main/java/basics/geekforgeeks/searching/SearchProblems.java
+++ src/main/java/basics/geekforgeeks/searching/SearchProblems.java
@@ -39,11 +39,41 @@ public class SearchProblems {
         }
     }
 
-    public static void main(String[] args) {
-        var nums = new int[]{1, 2, 3, 4, 5, 6};
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println(binarySearchIterative(nums, i));
-            System.out.println(binarySearchRecursive(nums, i));
+    static int indexOfFirstOccurrenceInSortedArray(int[] nums, int searchKey) {
+        int i = 0, j = nums.length - 1;
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            int value = nums[mid];
+            System.out.println("mid="+mid+", value="+value);
+            if (value > searchKey) {
+                j = mid - 1;
+            } else if (value < searchKey) {
+                i = mid + 1;
+            } else {
+                if (mid == 0 || nums[mid - 1] != searchKey) {
+                    return mid;
+                } else {
+                    j = mid - 1;
+                }
+            }
         }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        var nums = new int[]{1,2,3,3,3,4,4,5,6,6,6,7};
+        for (int i = 1; i < 2; i++) {
+            System.out.println("first index of "+i +"="+indexOfFirstOccurrenceInSortedArray(nums, i));
+        }
+    }
+
+    static void testBinarySearch(){
+        var nums = new int[]{10, 20, 30, 40, 50, 60};
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(binarySearchIterative(nums, i * 10));
+            System.out.println(binarySearchRecursive(nums, i * 10));
+        }
+        System.out.println(binarySearchRecursive(nums, 555));
+        System.out.println(binarySearchIterative(nums, 555));
     }
 }
