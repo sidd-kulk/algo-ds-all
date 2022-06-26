d0e0b665dd122daf362a706a1132fb100ef34f21
Siddharth Kulkarni <sidcool1234@gmail.com>
2022-06-26 17:38:47 +0530

    Dynamic Programming - Kick off

--- src/main/java/basics/geekforgeeks/dynamicprogramming/DynamicProgrammingProblems.java
+++ src/main/java/basics/geekforgeeks/dynamicprogramming/DynamicProgrammingProblems.java
@@ -1,2 +1,41 @@
-package basics.geekforgeeks.dynamicprogramming;public class DynamicProgrammingProblems {
+package basics.geekforgeeks.dynamicprogramming;
+
+public class DynamicProgrammingProblems {
+
+    static int fibonacciMemoization(int n) {
+        return fibonacciMemoization(n, null);
+    }
+
+    static int fibonacciMemoization(int n, Integer[] memo) {
+        if (n < 0) throw new RuntimeException("Negative input");
+        if (n == 0 || n == 1) return n;
+
+        if (memo == null) {
+            memo = new Integer[n + 1];
+        }
+
+        if (memo != null && memo[n] != null) {
+            return memo[n];
+        }
+
+        memo[n] = fibonacciMemoization(n - 1) + fibonacciMemoization(n - 2);
+        return memo[n];
+    }
+
+    static int fiboniacciTabulation(int n) {
+        int[] table = new int[n + 1];
+        table[0] = 0;
+        table[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            table[i] = table[i - 1] + table[i - 2];
+        }
+        return table[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Memoized solution = "+fibonacciMemoization(8));
+        System.out.println("Tabulation solution = "+fiboniacciTabulation(8));
+    }
+
 }
