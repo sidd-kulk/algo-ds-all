4454d7199811ee2a1be943617c096a70d111eaa7
Siddharth Kulkarni <sidcool1234@gmail.com>
2022-07-05 13:45:27 +0530

    Strings - Check if String is subsequence

--- src/main/java/basics/geekforgeeks/strings/StringProblems.java
+++ src/main/java/basics/geekforgeeks/strings/StringProblems.java
@@ -37,7 +37,38 @@ public class StringProblems {
         return counts;
     }
 
+    static boolean checkStringSubsequenceIterative(String s1, String s2) {
+        if (s1 == null || s2 == null) return false;
+        int i = 0, j = 0;
+        while (i < s1.length() && j < s2.length()) {
+            if (s1.charAt(i) == s2.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        if (j == s2.length()) return true;
+        else return false;
+    }
+
+    static boolean checkStringSubsequenceRecursive(String s1, String s2) {
+        return checkStringSubsequenceRecursive(s1, s2, 0, 0);
+    }
+
+    private static boolean checkStringSubsequenceRecursive(String s1, String s2, int i, int j) {
+        if(j == s2.length()) return true;
+        if(i == s1.length()) return false;
+
+        if (s1.charAt(i) == s2.charAt(j)) {
+            return checkStringSubsequenceRecursive(s1, s2, ++i, ++j);
+        } else {
+            return checkStringSubsequenceRecursive(s1, s2, ++i, j);
+        }
+    }
+
     public static void main(String[] args) {
-        printSorted("dcba");
+        System.out.println(checkStringSubsequenceRecursive("geekforgeeks", "ge"));
     }
 }
